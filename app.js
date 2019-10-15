abdd9d0d16ea47b92837822e39d9d08d7b1fb9cf
cfabregas <cristian.fabregas@gmail.com>
2019-10-15 11:04:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
-function calculadora(primerNumero, segundoNumero){
-  
-var suma = primerNumero + segundoNumero;
-var resta = primerNumero - segundoNumero;
-var multiplicar = primerNumero * segundoNumero;
-  
-alert("El resultado de la suma da " + suma);
-alert("El resultado de la resta da " + resta);
-alert("El resultado de la multiplicación da " + multiplicar);
-}
+//exercici 1//
+console.log("Hola Mundo");
 
-var numero1 = 3;
-var numero2 = 5;
+//exercici 2//
+alert("¡Me llamo Cristian!");
+var a = "¡Mi apellido es Fàbregas!";
+alert(a);
 
-calculadora(numero1, numero2);
-
-console.log("Hola Mundo")
-
-alert("¡Me llamo Cristian!")
-var a = "¡Mi apellido es Fàbregas!"
-alert( a )
-
+//exercici 3//
 var b = "Cristian"
 var c = " Fàbregas"
-console.log( b+c )
+console.log(b+c);
 
+//exercici 4//
 var num1 = 48
 var num2 = 62
 suma = num1 + num2
-console.log("La suma entre los números es: "+suma)
+console.log("La suma entre los números es: "+suma);
 
+//exercici 5//
 var nota_examen = 9.8;
 if (nota_examen >= 5) {
 alert("Felicidades, has aprovado el examen con un "+nota_examen);
@@ -37,6 +26,7 @@ else {
 alert("Mecachis, has suspendido el examen con un "+nota_examen);
 }
 
+//exercici 6//
 var d = "Tinc un cotxe de color verd"
 console.log( d )
 
@@ -48,9 +38,23 @@ var frase2 = "Tinc un cotxe de color blau";
 frase2 = frase2.replace(/o/gi,'u');
 console.log(frase2);
 
+//xercisi 7//
 var objeto = ["Mesa", "Silla", "Ordenador", "Libreta"];
 var contador;
 for( contador=0; contador < 4; contador++ ) {
 console.log( "El objeto " + objeto[contador] + " está en la posición " + contador + "  " ); 
 }
 
+//exercici 8//
+function calculadora(operador, valor1, valor2){
+if (operador == "suma"){
+    return "El resultado de la suma entre " +valor1+ " y " +valor2+ " da como resultado " +(parseInt(valor1) + parseInt(valor2));
+} 
+else if (operador == "resta"){
+    return "El resultado de la resta entre " +valor1+ " y " +valor2+ " da como resultado " +(parseInt(valor1)-parseInt(valor2));
+}    
+else if (operador == "multipl"){
+    return "El resultado de la multiplicación entre " +valor1+ " y " +valor2+ " da como resultado " +valor1*valor2;
+}
+}
+alert (calculadora("resta",20,5))
